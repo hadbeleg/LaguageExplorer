e8ba9bb9a4f59e0f5ca901184be46cc603e64be3
hadbeleg <nhadbeleg@gmail.com>
2024-02-23 18:29:40 +0900

    yahaw de

--- app/SignUp.js
+++ app/SignUp.js
@@ -1,101 +1,14 @@
-import React, { useRef } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const SignUp = () => {
-  const emailInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
-
-  const focusEmailInput = () => {
-    if (emailInputRef.current) {
-      emailInputRef.current.focus();
-    }
-  };
-
-  const focusPasswordInput = () => {
-    if (passwordInputRef.current) {
-      passwordInputRef.current.focus();
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>SignUp</Text>
-      <Text style={styles.inputLabel} onPress={focusEmailInput}>
-        Email Address
-      </Text>
-      <TextInput
-        ref={emailInputRef}
-        style={styles.input}
-        keyboardType="email-address"
-        placeholder="Enter your email"
-        autoCapitalize="none"
-      />
-      <Text style={styles.inputLabel} onPress={focusPasswordInput}>
-        Password
-      </Text>
-      <TextInput
-        ref={passwordInputRef}
-        style={styles.input}
-        secureTextEntry
-        placeholder="Enter your password"
-      />
-      <TouchableOpacity style={styles.button}>
-        <Link href="/Signin">
-          <Text style={styles.buttonText}>Go to Login</Text>
-        </Link>
-      </TouchableOpacity>
+    <View>
+      <Text>SignUp</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ff7300",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    color: "white",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  inputLabel: {
-    color: "white",
-    marginBottom: 5,
-    textDecorationLine: "underline",
-  },
-  input: {
-    height: 40,
-    width: 250,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    backgroundColor: "#fff",
-  },
-  button: {
-    backgroundColor: "#FFD700",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    width: 200,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-});
-
 export default SignUp;
+
+const styles = StyleSheet.create({});
--- app/Signin.js
+++ app/Signin.js
@@ -1,108 +1,14 @@
-import React, { useRef } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
-import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Signin = () => {
-  const emailInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
-
-  const focusEmailInput = () => {
-    if (emailInputRef.current) {
-      emailInputRef.current.focus();
-    }
-  };
-
-  const focusPasswordInput = () => {
-    if (passwordInputRef.current) {
-      passwordInputRef.current.focus();
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Signin</Text>
-      <View>
-        <TouchableOpacity onPress={focusEmailInput}>
-          <Text style={styles.inputLabel}>Email Address</Text>
-        </TouchableOpacity>
-        <TextInput
-          ref={emailInputRef}
-          style={styles.input}
-          keyboardType="email-address"
-          placeholder="Enter your email"
-          autoCapitalize="none"
-        />
-      </View>
-      <View>
-        <TouchableOpacity onPress={focusPasswordInput}>
-          <Text style={styles.inputLabel}>Password</Text>
-        </TouchableOpacity>
-        <TextInput
-          ref={passwordInputRef}
-          style={styles.input}
-          secureTextEntry
-          placeholder="Enter your password"
-        />
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Link href="/SignUp">
-          <Text style={styles.buttonText}>Go to SignUp</Text>
-        </Link>
-      </TouchableOpacity>
+    <View>
+      <Text>Signin</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ff7300",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    backgroundColor: "#FFD700",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 10,
-    color: "white",
-  },
-  inputLabel: {
-    color: "white",
-    marginBottom: 5,
-  },
-  input: {
-    height: 40,
-    width: 250,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    backgroundColor: "#fff",
-  },
-  button: {
-    backgroundColor: "#FFD700",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    width: 200,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-});
-
 export default Signin;
+
+const styles = StyleSheet.create({});
